69d4f1ac9a21dd9894ef123a4724d55f885fa841
medazza <midazza27@gmail.com>
2024-01-03 00:42:06 -0800

    fix betty error at task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,8 +15,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!nwNode)
 		return (NULL);
 	if (parent->left != NULL)
+	{
 		nwNode->left = parent->left;
 		parent->left->parent = nwNode;
+	}
 	parent->left = nwNode;
 	return (nwNode);
 }
+
