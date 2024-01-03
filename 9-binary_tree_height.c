d31f39a3d0a1c5d069d2c678a372fe4eba6797c4
medazza <midazza27@gmail.com>
2024-01-03 14:59:51 +0100

    task 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height -  creates a binary tree node.
+ *
+ * @tree: pointer to the parent node of the node to create.
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (!tree)
+		return (0);
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (left_height > right_height ? left_height : right_height);
+}
