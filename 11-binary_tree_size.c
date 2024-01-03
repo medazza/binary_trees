fda1e595070e140a01fb8947bbc88d21fc5b331c
medazza <midazza27@gmail.com>
2024-01-03 15:13:12 +0100

    task 11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size -  creates a binary tree node.
+ *
+ * @tree: pointer to the parent node of the node to create.
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
