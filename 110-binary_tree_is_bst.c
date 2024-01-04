e2cbda8c86902510f61a982eb1750f878d877623
medazza <midazza27@gmail.com>
2024-01-04 16:16:18 +0100

    task 24

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * is_bst_util - Function help to check if a binary tree is a valid BST.
+ * @tree: Pointer to the root node of the tree to check.
+ * @min: The minimum value a node in the tree can have.
+ * @max: The maximum value a node in the tree can have.
+ * Return: 1 if the tree is a valid BST, 0 otherwise.
+ */
+int is_bst_util(const binary_tree_t *tree, int min, int max)
+{
+	if (!tree)
+		return (1);
+	if (tree->n <= min || tree->n >= max)
+		return (0);
+	return (is_bst_util(tree->left, min, tree->n) &&
+		is_bst_util(tree->right, tree->n, max));
+}
+
+/**
+ * binary_tree_is_bst - func checks if a binary tree is a
+ * valid Binary Search Tree (BST).
+ * @tree: Pointer to the root node of the tree to check.
+ * Return: 1 if the tree is a valid BST, 0 otherwise.
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (is_bst_util(tree, INT_MIN, INT_MAX));
+}
