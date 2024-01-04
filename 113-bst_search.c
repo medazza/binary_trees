cbd0bf85595fb3aa240dad68444b2d81990b532b
medazza <midazza27@gmail.com>
2024-01-04 16:37:01 +0100

    task 27

---
+++ 113-bst_search.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * bst_search - func search for a value in a Binary Search Tree.
+ * @tree: Pointer to the root node of the BST to search.
+ * @value: The value to search for.
+ * Return: Pointer to the node containing the
+ * value, or NULL if not found.
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (tree == NULL)
+		return (NULL);
+	if (tree->n == value)
+		return ((bst_t *)tree);
+	if (value < tree->n)
+		return (bst_search(tree->left, value));
+	else
+		return (bst_search(tree->right, value));
+}
