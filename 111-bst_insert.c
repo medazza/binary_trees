51c37c46d0c47ba7f4a4b6c38dcbe44feae2a05c
medazza <midazza27@gmail.com>
2024-01-04 16:24:10 +0100

    task 25

---
+++ 111-bst_insert.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+
+/**
+ * bst_insert - func inserts a value in a Binary Search Tree (BST).
+ * @tree: A double pointer to the root node of the BST.
+ * @value: The value to insert in the tree.
+ * Return: A pointer to the created node, or NULL on failure.
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	if (tree == NULL)
+		return (NULL);
+	if (*tree == NULL)
+	{
+		*tree = binary_tree_node(NULL, value);
+		return (*tree);
+	}
+	if (value < (*tree)->n)
+	{
+		if ((*tree)->left == NULL)
+		{
+			(*tree)->left = binary_tree_node(*tree, value);
+			return ((*tree)->left);
+		}
+		return (bst_insert(&((*tree)->left), value));
+	}
+	if (value > (*tree)->n)
+	{
+		if ((*tree)->right == NULL)
+		{
+			(*tree)->right = binary_tree_node(*tree, value);
+			return ((*tree)->right);
+		}
+		return (bst_insert(&((*tree)->right), value));
+	}
+	return (NULL);
+}
